6f942b201cf990d7f101d9784831ca4eb2058a00
Michael Maye <michael.maye@harrowingpleasantries.com>
2023-07-23 18:59:51 -0400

    added cardview and SQLite database to CSC475 Module 5

--- CSC475/src/Modules/Module5/StudentData/app/src/main/java/com/csc475/studentdata/MainActivity.java
+++ CSC475/src/Modules/Module5/StudentData/app/src/main/java/com/csc475/studentdata/MainActivity.java
@@ -1,8 +1,13 @@
 package com.csc475.studentdata;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.csc475.studentdata.database.StudentDataContract;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // Assuming you have the list of students from your SQLite database
+        ArrayList<Student> studentList = // Get the ArrayList of Student objects from your database
+
+                StudentAdapter studentAdapter = new StudentAdapter(this, studentList);
+        recyclerView.setAdapter(studentAdapter);
+
     }
 }
